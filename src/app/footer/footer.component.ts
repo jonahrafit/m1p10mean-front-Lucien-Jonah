290743e0592cb589308fdf3bd60fad0c8f2f7967
jonahrafit <jonahrafit.ram@hotmail.com>
2024-02-16 00:53:38 +0300

    Modification header

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
+  // Détecte le défilement de la page pour afficher ou masquer le bouton de retour vers le haut
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY;
+    const backToTopButton = document.getElementById('back-to-top-button');
+    if (backToTopButton) {
+      if (scrollY >= 200) {
+        backToTopButton.style.display = 'block';
+      } else {
+        backToTopButton.style.display = 'none';
+      }
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,49 @@
-import { Component, Output } from '@angular/core';
 import { InscriptionComponent } from '../inscription/inscription.component';
 import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { Component, Output } from '@angular/core';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { ToastModule } from 'primeng/toast';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { DockModule } from 'primeng/dock';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    SpeedDialModule,
+    ToastModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    DockModule,
+    AutoCompleteModule,
+    FormsModule,
+    TableModule,
+    TagModule,
+    LoginComponent,
+    CommonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   @Output() visible: boolean = false;
+  isLoggedIn: boolean = false; // Ajout d'une variable pour suivre l'état de connexion de l'utilisateur
+  user: any = {
+    nom: 'Jonah',
+    prenom: 'Fitia'
+  }; // Ajout d'une variable pour stocker les détails de l'utilisateur connecté
 
   constructor(private dialog: MatDialog) { }
 
